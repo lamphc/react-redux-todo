28bd9385a835cab066052f6d9a8e620ab4c19718
lamphc <lamphc@live.com>
2020-03-19 18:45:10 +0800

    Updates

---
+++ precache-manifest.35f23cf89cc7dafa1e933790ac145e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae5975e849f5c3e4ca0a0c667e16d50",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "7095e8de842fbe626af0",
+    "url": "/react-redux-todo/static/css/main.09ede2b7.chunk.css"
+  },
+  {
+    "revision": "724054a35d7c29c093ca",
+    "url": "/react-redux-todo/static/js/2.5a71efc4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-redux-todo/static/js/2.5a71efc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7095e8de842fbe626af0",
+    "url": "/react-redux-todo/static/js/main.62baa2a7.chunk.js"
+  },
+  {
+    "revision": "94017f82178314ea582e",
+    "url": "/react-redux-todo/static/js/runtime-main.21b35403.js"
+  }
+]);
