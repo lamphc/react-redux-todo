01345a602320faf517c6e4a38dffebce00dea9bf
lamphc <lamphc@live.com>
2020-08-05 21:26:04 +0800

    add devtools code

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,11 @@
  * 配置store
  */
 
-import { createStore } from "redux";
-import reducer from "./reducer";
+import { createStore } from "redux"
+import reducer from "./reducer"
 
-export default function configStore() {
-  const store = createStore(reducer);
+export default function configStore () {
+  const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   console.log(store)
   // 监听变化
   store.subscribe(() => {
